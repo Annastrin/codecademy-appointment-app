fbdda84d0188ac4024f487f1b5b37fdba1815493
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-01-16 15:31:31 +1300

    Update Contact Form with input components

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Alert, Button } from "@mui/material";
+import TextInput from "../formInputs/textInput/TextInput";
+import PhoneInput from "../formInputs/phoneInput/PhoneInput";
+import EmailInput from "../formInputs/emailInput/EmailInput";
 import { Contact } from "../../types";
 
 type ContactFormProps = {
@@ -16,24 +21,20 @@ export default function ContactForm({
   contacts,
   addContact,
 }: ContactFormProps) {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<Inputs>();
+  const methods = useForm<Inputs>();
   const [contactExistsError, setContactExistsError] = useState(false);
 
   function onSubmit(data: Inputs) {
+    console.log(data);
     setContactExistsError(false);
     const newContact = {
       name: data.name,
       phone: data.phone,
       email: data.email,
     };
-    if (!contactInContacts(newContact, contacts)) {
+    if (!contactAlreadyExists(newContact, contacts)) {
       addContact(newContact);
-      reset();
+      methods.reset();
     } else {
       setContactExistsError(true);
     }
@@ -44,74 +45,39 @@ export default function ContactForm({
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-field">
-        <input
-          type="text"
-          {...register("name", { required: true })}
-          placeholder="Full Name"
-        />
-        {errors.name && (
-          <span className="error-message">This field is required</span>
-        )}
-      </div>
-
-      <div className="form-field">
-        <input
-          type="tel"
-          {...register("phone", {
-            required: true,
-            pattern:
-              /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/gim,
-          })}
-          placeholder="Phone number"
-        />
-        {errors.phone?.type === "required" && (
-          <span className="error-message">This field is required</span>
-        )}
-        {errors.phone?.type === "pattern" && (
-          <span className="error-message">Provide a correct phone number</span>
-        )}
-      </div>
-
-      <div className="form-field">
-        <input
-          type="email"
-          {...register("email", {
-            required: true,
-            pattern:
-              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm,
-          })}
-          placeholder="Email"
-        />
-        {errors.email?.type === "required" && (
-          <span className="error-message">This field is required</span>
-        )}
-        {errors.email?.type === "pattern" && (
-          <span className="error-message">Provide a correct email</span>
-        )}
-      </div>
-
-      <input type="submit" value="Add" />
-      {contactExistsError && (
-        <div>
-          <span className="error-message">This contact already exists</span>
-          <button onClick={closeContactExistsError}>X</button>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
+        <div className="form-field">
+          <TextInput name="name" required label="Full Name" />
         </div>
-      )}
-    </form>
+        <div className="form-field">
+          <PhoneInput name="phone" required label="Phone Number" />
+        </div>
+        <div className="form-field">
+          <EmailInput name="email" required label="Email" />
+        </div>
+
+        <Button variant="contained" type="submit">
+          Add
+        </Button>
+        {contactExistsError && (
+          <Alert
+            severity="error"
+            onClose={closeContactExistsError}
+            sx={{ marginTop: 1 }}
+          >
+            This contact already exists!
+          </Alert>
+        )}
+      </form>
+    </FormProvider>
   );
 }
 
-function contactInContacts(contact: Contact, contacts: Contact[]) {
+function contactAlreadyExists(contact: Contact, contacts: Contact[]) {
   for (let i = 0; i < contacts.length; i++) {
     if (contact.name === contacts[i].name) {
-      if (
-        contact.phone === contacts[i].phone &&
-        contact.email === contacts[i].email
-      ) {
-        return true;
-      }
+      return true;
     }
   }
   return false;
