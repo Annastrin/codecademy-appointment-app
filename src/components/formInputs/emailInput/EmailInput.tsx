e8f3150f4ea79af64931ca46739bbf4dad253444
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-01-28 21:40:02 +1300

    Disable eslint email pattern validation

--- src/components/formInputs/emailInput/EmailInput.tsx
+++ src/components/formInputs/emailInput/EmailInput.tsx
@@ -11,7 +11,7 @@ export default function EmailInput({ name, label, id, required }: InputProps) {
       rules={{
         required: required,
         pattern:
-          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm,
+          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm, // eslint-disable-line
       }}
       render={({ fieldState, field }) => (
         <TextField
