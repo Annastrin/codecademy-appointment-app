b67e45aa73e54afa6ed643849d157bfa8908d48a
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-01-30 22:10:57 +1300

    Change some other routes with public_url

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 import AppointmentsPage from "./containers/appointmentsPage/AppointmentsPage";
 import ContactsPage from "./containers/contactsPage/ContactsPage";
 import HomePage from "./containers/homePage/HomePage";
@@ -8,11 +9,18 @@ export default function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<HomePage />}>
+        <Route path={routes.default} element={<HomePage />}>
           <Route index element={<ContactsPage />} />
           <Route path={routes.contacts} element={<ContactsPage />} />
           <Route path={routes.appointments} element={<AppointmentsPage />} />
-          <Route path="*" element={<p>Page not found</p>} />
+          <Route
+            path="*"
+            element={
+              <Typography variant="h2" align="center">
+                Page not found
+              </Typography>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
