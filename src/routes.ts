e83c0b547fff487b282d10287429a2d41b676ce8
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-01-30 22:20:00 +1300

    Remove public urls from routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 export const routes = {
-  default: `${process.env.PUBLIC_URL}/`,
-  contacts: `${process.env.PUBLIC_URL}/contacts`,
-  appointments: `${process.env.PUBLIC_URL}/appointments`,
+  default: `/`,
+  contacts: `/contacts`,
+  appointments: `/appointments`,
 };
